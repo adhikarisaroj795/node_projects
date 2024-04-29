9e52205c8af9215fdf0dff2d615d3a16f3410d2a
saroj adhikari <adhikarisaroj291@gmail.com>
2024-04-29 23:20:07 +0545

    responsive navbar

---
+++ portFolio/portfolio/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./components/header/Header";
+import Home from "./components/home/Home";
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Home />
+    </>
+  );
+};
+
+export default App;
