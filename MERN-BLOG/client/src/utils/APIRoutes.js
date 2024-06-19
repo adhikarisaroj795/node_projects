443d0f5f0c8158cab1d41a5d835e04032f8fd4dc
saroj adhikari <adhikarisaroj291@gmail.com>
2024-06-19 22:55:24 +0545

    delete frontend

--- MERN-BLOG/client/src/utils/APIRoutes.js
+++ MERN-BLOG/client/src/utils/APIRoutes.js
@@ -4,3 +4,4 @@ export const signUpRoute = `${host}/api/v1/sign-up`;
 export const signInRoute = `${host}/api/v1/sign-in`;
 export const googleAuthRoute = `${host}/api/v1/auth/google`;
 export const updateUser = `${host}/api/v1/user/update`;
+export const deleteUser = `${host}/api/v1/user/delete`;
