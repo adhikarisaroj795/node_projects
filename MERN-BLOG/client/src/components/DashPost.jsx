900e706ca387f1e666eb4e29f6e1274d009656a5
saroj adhikari <adhikarisaroj291@gmail.com>
2024-06-27 21:26:12 +0545

    show user post inside dashboard

--- MERN-BLOG/client/src/components/DashPost.jsx
+++ MERN-BLOG/client/src/components/DashPost.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { fetchPostsRoute } from "../utils/APIRoutes";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 import { Table } from "flowbite-react";
 
 const DashPost = () => {
@@ -40,7 +41,7 @@ const DashPost = () => {
   }
 
   return (
-    <div>
+    <div className="table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500">
       {currentUser.user.isAdmin && userPosts.length > 0 ? (
         <>
           <Table hoverable className="shadow-md">
@@ -52,11 +53,46 @@ const DashPost = () => {
               <Table.HeadCell>Delete</Table.HeadCell>
               <Table.HeadCell>Edit</Table.HeadCell>
             </Table.Head>
-            <Table.Body>
+            <Table.Body className="divide-y">
               {userPosts.map((post) => (
-                <Table.Row key={post._id}>
-                  <Table.Cell>{post.updatedAt}</Table.Cell>
-                  {/* Add other cells here */}
+                <Table.Row
+                  className="bg-white dark:border-gray-700 dark:bg-gray-800"
+                  key={post._id}
+                >
+                  <Table.Cell>
+                    {new Date(post.updatedAt).toLocaleDateString()}
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Link to={`/post/${post.slug}`}>
+                      <img
+                        src={post.image}
+                        alt={post.title}
+                        className="w-20 h-10 object-cover bg-gray-500"
+                      />
+                    </Link>
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Link
+                      className="font-medium text-red-900 dark:text-white"
+                      to={`/post/${post.slug}`}
+                    >
+                      {post.title}
+                    </Link>
+                  </Table.Cell>
+                  <Table.Cell>{post.category}</Table.Cell>
+                  <Table.Cell>
+                    <span className="font-medium text-red-500 hover:underline cursor-pointer">
+                      Delete
+                    </span>
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Link
+                      className="text-teal-500 hover:underline"
+                      to={`/update-post${post._id}`}
+                    >
+                      <span>Edit</span>
+                    </Link>
+                  </Table.Cell>
                 </Table.Row>
               ))}
             </Table.Body>
@@ -70,3 +106,5 @@ const DashPost = () => {
 };
 
 export default DashPost;
+
+//5:56
