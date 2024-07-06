cf3a9cf3303891725f232a73c2d208f7d92b55af
saroj adhikari <adhikarisaroj291@gmail.com>
2024-07-06 21:44:09 +0545

    addd recent article section to the post page

--- MERN-BLOG/client/src/pages/PostPage.jsx
+++ MERN-BLOG/client/src/pages/PostPage.jsx
@@ -4,12 +4,14 @@ import { fetchPostsRoute } from "../utils/APIRoutes.js";
 import { Button, Spinner } from "flowbite-react";
 import CallToAction from "../components/CallToAction.jsx";
 import CommentSection from "../components/CommentSection.jsx";
+import PostCard from "../components/PostCard.jsx";
 
 const PostPage = () => {
   const { postSlug } = useParams();
   const [loading, setIsloading] = useState(true);
   const [error, setError] = useState(false);
   const [post, setPost] = useState(null);
+  const [recentPosts, setRecentPosts] = useState(null);
 
   useEffect(() => {
     const fetchPost = async () => {
@@ -38,6 +40,22 @@ const PostPage = () => {
     };
     fetchPost();
   }, [postSlug]);
+
+  useEffect(() => {
+    try {
+      const fetchRecentPosts = async () => {
+        const res = await fetch(`${fetchPostsRoute}?limit=3`);
+        const data = await res.json();
+
+        if (res.ok) {
+          setRecentPosts(data.posts);
+        }
+      };
+      fetchRecentPosts();
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
   if (loading)
     return (
       <div className="flex justify-center items-center min-h-screen">
@@ -76,6 +94,14 @@ const PostPage = () => {
         <CallToAction />
       </div>
       <CommentSection postId={post._id} />
+
+      <div className="flex flex-col justify-center items-center mb-5">
+        <h1 className="text-xl mt-5">Recent articles</h1>
+        <div className="flex flex-wrap gap-5 mt-5 justify-center">
+          {recentPosts &&
+            recentPosts.map((post) => <PostCard key={post._id} post={post} />)}
+        </div>
+      </div>
     </main>
   );
 };
