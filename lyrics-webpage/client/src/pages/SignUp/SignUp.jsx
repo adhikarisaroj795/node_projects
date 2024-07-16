3773de50f4454a371ba36f57d00fd4dfa9c2a172
saroj adhikari <adhikarisaroj291@gmail.com>
2024-07-16 17:28:41 +0545

    add signup functionalities to the frontend

--- lyrics-webpage/client/src/pages/SignUp/SignUp.jsx
+++ lyrics-webpage/client/src/pages/SignUp/SignUp.jsx
@@ -1,25 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
 import "../Login/Login";
 import "./signUp.css";
-import { Link } from "react-router-dom";
+import { signUpRoutes } from "../../utils/APIRoutes";
+import { Link, useNavigate } from "react-router-dom";
 
 const SignUp = () => {
+  const [userData, setUserData] = useState({
+    fullname: "",
+    email: "",
+    password: "",
+  });
+
+  const [errMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setUserData({
+      ...userData,
+      [e.target.id]: e.target.value.trim(),
+    });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!userData.fullname || !userData.email || !userData.password) {
+      return setErrorMessage("Please fill all the input");
+    }
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch(signUpRoutes, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(userData),
+      });
+      const data = await res.json();
+      if (data.status === true) {
+        navigate("/login");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <>
       <div className="container section login-holder row-rev">
         <div className="login-form ">
           <h2 className="m-b-25">Sign Up</h2>
-          <form>
+          <form onSubmit={handleSubmit}>
             <label>Full name</label>
-            <input type="text" placeholder="Enter Your Name" />
+            <input
+              type="text"
+              placeholder="Enter Your Name"
+              onChange={handleChange}
+              id="fullname"
+            />
             <label>Email</label>
-            <input type="text" placeholder="Enter Your Email" />
+            <input
+              type="text"
+              placeholder="Enter Your Email"
+              onChange={handleChange}
+              id="email"
+            />
             <label>Password</label>
-            <input type="text" placeholder="Enter Your Password" />
+            <input
+              type="text"
+              placeholder="Enter Your Password"
+              onChange={handleChange}
+              id="password"
+            />
             <span>
               already have an account <Link to="/login">login?</Link>
             </span>
-            <button className="song-button">Login</button>
+            <button className="song-button" type="submit">
+              Login
+            </button>
           </form>
         </div>
         <div className="login-form bg-logo">
---
+++ lyrics-webpage/client/src/utils/APIRoutes.js
@@ -0,0 +1,2 @@
+export const host = "http://localhost:2424";
+export const signUpRoutes = `${host}/api/v1/sign-up`;
